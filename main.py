e386338c88d6c5f6bd4871f568b27d3844a8f548
shingonati0n <hsmunozc@gmail.com>
2024-09-11 20:06:21 -0300

    Initial testing of start and stop container - create container doesn't work yet

--- main.py
+++ main.py
@@ -1,80 +1,142 @@
+#from Plugin
 from pathlib import Path
 import subprocess
 import sys
 import os
 import asyncio
 import decky
+#from original program
+import socket
+import ipaddress
+import struct
+import shlex
+import time 
+import io
+import configparser
+from subprocess import Popen, PIPE, CalledProcessError
+from getpass import getpass
 
+#append py_modules to PYTHONPATH
+sys.path.append(str(Path(__file__).parent / "py_modules"))
+
+from lib import omegaWorker
+
+thisusr = decky.USER
+thisusrhome = decky.DECKY_USER_HOME
+
+#static stuff
+roadsto14 = [
+	"124.150.157.0/24",
+	"153.254.80.0/24",
+	"202.67.52.0/24",
+	"204.2.29.0/24",
+	"80.239.145.0/24"]
+
+#get subnet mask and subnet
+def cidr_to_netmask(cidr):
+	network, net_bits = cidr.split('/')
+	host_bits = 32 - int(net_bits)
+	netmask = socket.inet_ntoa(struct.pack('!I',(1 << 32) - (1 << host_bits)))
+	return network, netmask
+
+#validate IP Address
+def is_valid_ipv4_address(address):
+	try:
+		socket.inet_pton(socket.AF_INET, address)
+	except AttributeError:  # no inet_pton here, sorry
+		try:
+			socket.inet_aton(address)
+		except socket.error:
+			return False
+		return address.count('.') == 3
+	except socket.error:  # not a valid address
+		return False
+	return True
+
+#Plugin code
 class Plugin:
 
-    _enabled = False
+	_enabled = False
+	
+	# Current Status
+	async def curr_status(self)-> bool:
+		decky.logger.info(Plugin._enabled)
+		return Plugin._enabled
 
-    # Current Status
-    async def curr_status(self)-> bool:
-        decky.logger.info("Informing Status")
-        return Plugin._enabled
+	#switch toggle status
+	async def toggle_status(self, check):
+		decky.logger.info(check)
+		decky.logger.info(type(check))
+		if Plugin._enabled == True and check['checkd'] == False:
+			decky.logger.info("Switched via toggle_status")
+			#omegaBeetle.SelfDestructProtocol()
+			omegaBeetle.testStop()
+		Plugin._enabled = check['checkd']
+	
+	#function for onKill
+	async def stop_status(self):
+		Plugin._enabled = False
+		decky.logger.info("Killing on the way out")
 
-    #switch toggle status
-    async def toggle_status(self, check):
-        decky.logger.info(check)
-        decky.logger.info(type(check))
-        if Plugin._enabled == True and check == False:
-            decky.logger.info("Switched via toggle_status")
-        Plugin._enabled = check
-    
-    #function for onKill
-    async def stop_status(self):
-        Plugin._enabled = False
-        decky.logger.info("Killing on the way out")
+	# Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+	async def _main(self):
+		omegaBeetle = omegaWorker.WorkerClass()
+		# try:
+		# 	xivomega = subprocess.Popen(str(Path(decky.DECKY_PLUGIN_DIR) / "omega_create.sh"))
+		# 	if xivomega.returncode == 0:
+		# 		decky.logger.info("podman container created successfully")
+		# except subprocess.CalledProcessError as e:
+		# 	pass
+		# 	decky.logger.info(e.stdout.decode())
+		# Omega Code will go here 
+		# ToDo: 
+		# Copy container storage into home folder under xivomega_cont // add select path for this
+		# Modify /etc/containers/storage.conf 
+		# create ipvlan and podman ipvlan network
+		# create container 
+		decky.logger.info("Starting main program")
+		decky.logger.info(thisusr)
+		decky.logger.info(thisusrhome)
+		omegaBeetle.testStart()
+		omegaBeetle.podmanInfo()
 
+	# Enable services
+	# async def enable(self):
+	#     Plugin._enabled = True
 
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
-    async def _main(self):
-        # Omega Code will go here 
-        # ToDo: 
-        # Copy container storage into home folder under xivomega_cont 
-        # Modify /etc/containers/storage.conf 
-        # create ipvlan and podman ipvlan network
-        # create container 
-        decky.logger.info("Entering inside main")
+	# Disable services
+	# async def disable(self):
+	#     Plugin._enabled = False
 
-    # Enable services
-    # async def enable(self):
-    #     Plugin._enabled = True
+		# Function called first during the unload process, utilize this to handle your plugin being stopped, but not
+	# completely removed
+	async def _unload(self):
+		Plugin.stop_status(self)
+		decky.logger.info("Goodnight World!")
+		pass
 
-    # Disable services
-    # async def disable(self):
-    #     Plugin._enabled = False
+	# Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
+	# plugin that may remain on the system
+	async def _uninstall(self):
+		decky.logger.info("Goodbye World!")
+		pass
 
-        # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
-    # completely removed
-    async def _unload(self):
-        Plugin.stop_status(self)
-        decky.logger.info("Goodnight World!")
-        pass
-
-    # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
-    # plugin that may remain on the system
-    async def _uninstall(self):
-        decky.logger.info("Goodbye World!")
-        pass
-
-    # Migrations that should be performed before entering `_main()`.
-    async def _migration(self):
-        decky.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky.migrate_settings(
-            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky.migrate_runtime(
-            os.path.join(decky.DECKY_HOME, "template"),
-            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+	# Migrations that should be performed before entering `_main()`.
+	async def _migration(self):
+		decky.logger.info("Migrating")
+		# Here's a migration example for logs:
+		# - `~/.config/decky-template/template.log` will be migrated to `decky.DECKY_PLUGIN_LOG_DIR/template.log`
+		decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
+											   ".config", "decky-template", "template.log"))
+		# Here's a migration example for settings:
+		# - `~/homebrew/settings/template.json` is migrated to `decky.DECKY_PLUGIN_SETTINGS_DIR/template.json`
+		# - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_SETTINGS_DIR/`
+		decky.migrate_settings(
+			os.path.join(decky.DECKY_HOME, "settings", "template.json"),
+			os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
+		# Here's a migration example for runtime data:
+		# - `~/homebrew/template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_RUNTIME_DIR/`
+		# - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.DECKY_PLUGIN_RUNTIME_DIR/`
+		decky.migrate_runtime(
+			os.path.join(decky.DECKY_HOME, "template"),
+			os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
---
+++ py_modules/lib/omegaWorker.py
@@ -0,0 +1,266 @@
+#from Plugin
+from pathlib import Path
+import subprocess
+import sys
+import os
+import decky
+#from original program
+import socket
+import ipaddress
+import struct
+import shlex
+import time 
+import io
+import configparser
+from subprocess import Popen, PIPE, CalledProcessError
+from getpass import getpass
+
+class WorkerClass:
+	def SelfCreateProtocol(self,lip):
+		omegapod = f"""podman create \
+		  --replace \
+		  --name=xivomega \
+		  --ip=10.88.0.7 \
+		  --sysctl net.ipv4.ip_forward=1 \
+		  --sysctl net.ipv4.conf.all.route_localnet=1 \
+		  --net=podman \
+		  --cap-add=NET_RAW,NET_ADMIN \
+		  -ti quay.io/shingonati0n/xivomega:latest /bin/sh"""	
+		try:
+			xivomega = subprocess.run(shlex.split(omegapod),check=True,capture_output=True)
+			if xivomega.returncode == 0:
+				decky.logger.info("podman container created successfully")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		
+		hclosew = f"podman network connect xivlanc xivomega --ip={lip}"
+		try:
+			hclosew = subprocess.run(shlex.split(hclosew),check=True,capture_output=True)
+			if hclosew.returncode == 0:
+				decky.logger.info("hooked to podman ipvlan network")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			hworld = subprocess.run(shlex.split("podman start xivomega"),check=True,capture_output=True)
+			if hworld.returncode == 0:
+				decky.logger.info("XIVOmega says - Hello World")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+
+	def createIpVlanC(self,sdsubn,sdgway):
+		podnet = f"podman network create --subnet={sdsubn} --gateway={sdgway} --driver=ipvlan -o parent=wlan0 xivlanc"
+		try:
+			xivnet = subprocess.run(shlex.split(podnet),check=True,capture_output=True)
+			if xivnet.returncode == 0:
+				decky.logger.info("podman ipvlan network xivlanc has been created")
+		except subprocess.CalledProcessError as e: 
+			decky.logger.info(e.stderr.decode())
+	
+	def fixPodmanStorage(self):
+		podstorecmd = "cp /home/deck/xivomega/storage/storage.conf /etc/containers/storage.conf"
+		psf = subprocess.run(shlex.split(podstorecmd),check=True,capture_output=True)
+		try:
+		   	if psf.returncode==0:
+		   		decky.logger.info("/etc/containers/storage.conf was patched")
+		   		pass
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+
+	def SetRoutes(self,rt14):
+		for r in rt14:
+			way = f"ip route add {r} via 10.88.0.7"
+			nav = subprocess.run(shlex.split(way),check=True,capture_output=True)
+			try:
+		   		if nav.returncode==0:
+		   			decky.logger.info(f"route to {r} added")
+			except subprocess.CalledProcessError as e:
+				decky.logger.info(e.stderr.decode())
+
+	def ClearNetavarkRules(self):
+		subprocess.run(shlex.split("iptables -F INPUT"),check=True,capture_output=True)
+		subprocess.run(shlex.split("iptables -F FORWARD"),check=True,capture_output=True)
+		subprocess.run(shlex.split("iptables -F OUTPUT"),check=True,capture_output=True)
+
+	def PrintLogo(self):
+		subprocess.call(pth + "titleCard.sh")
+
+	def ReconnectProtocol(self):
+		subprocess.run(shlex.split("podman restart xivomega"),check=True,capture_output=True)
+		subprocess.run(shlex.split("podman exec xivomega iptables -t nat -F POSTROUTING"),check=True,capture_output=True)
+		subprocess.run(shlex.split("podman exec xivomega /home/iptset.sh"),check=True,capture_output=True)
+
+	def CDTimer(self):
+		for i in range(10, 0, -1):
+			decky.logger.info(i, end = ' \r')
+			time.sleep(1)
+
+	def CreateHostAdapter(self,virtual_ip,netbits,broadcast):
+		ipvl1 = f"ip link add xivlanh link wlan0 type ipvlan mode l2"
+		ipvl2 = f"ip addr add {virtual_ip}/{netbits} brd {broadcast} dev xivlanh"
+		ipvl3 = f"ip link set xivlanh up"
+		
+		try:
+			ipvlh1 = subprocess.run(shlex.split(ipvl1),check=True,capture_output=True)
+			if ipvlh1.returncode == 0:
+				decky.logger.info("host ipvlan interface created")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		
+		try:
+			ipvlh2 = subprocess.run(shlex.split(ipvl2),check=True,capture_output=True)
+			if ipvlh2.returncode == 0:
+				decky.logger.info(f"host ipvlan interface IP is {virtual_ip}")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		
+		try:
+			ipvlh3 = subprocess.run(shlex.split(ipvl3),check=True,capture_output=True)
+			if ipvlh3.returncode == 0:
+				decky.logger.info("host ipvlan interface is up")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+
+	def SelfDestructProtocol(self):
+		decky.logger.info("Terminating Mitigation Protocol and XIVOmega")
+		for r in roadsto14:
+			way = f"ip route del {r} via 10.88.0.7"
+			nav = subprocess.run(shlex.split(way),check=True,capture_output=True)
+			try:
+		   		if nav.returncode==0:
+		   			decky.logger.info(f"route to {r} deleted")
+			except subprocess.CalledProcessError as e:
+					decky.logger.info(e.stderr.decode())
+		try:
+			panto = subprocess.run(shlex.split("podman stop xivomega"),check=True,capture_output=True)
+			if panto.returncode == 0:
+				decky.logger.info("XIVOmega Container Stopped")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			atomic = subprocess.run(shlex.split("podman network disconnect xivlanc xivomega"),check=True,capture_output=True)
+			if atomic.returncode == 0:
+				decky.logger.info("XIVOmega IPVlan Disconnected")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			flame = subprocess.run(shlex.split("podman network rm xivlanc"),check=True,capture_output=True)
+			if flame.returncode == 0:
+				decky.logger.info("XIVOmega IPVlan Removed")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			bworld = subprocess.run(shlex.split("podman rm xivomega"),check=True,capture_output=True)
+			if bworld.returncode == 0:
+				decky.logger.info("XIVOmega Container removed")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			lanhdie = subprocess.run(shlex.split("ip link set xivlanh down"),check=True,capture_output=True)
+			if lanhdie.returncode == 0:
+				decky.logger.info("Host IPVlan turned off")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		try:
+			lanhrm = subprocess.run(shlex.split("ip link del xivlanh"),check=True,capture_output=True)
+			if lanhrm.returncode == 0:
+				decky.logger.info("Host IPVlan removed")
+		except subprocess.CalledProcessError as e:
+			decky.logger.info(e.stderr.decode())
+		decky.logger.info("All done - Goodbye")
+
+	def SelfCleaningProtocol(self):
+		ccnt = 0
+		for r in roadsto14:
+			way = f"ip route del {r} via 10.88.0.7"
+			try:
+				nav = subprocess.run(shlex.split(way),check=True,capture_output=True)
+				if nav.returncode==0:
+		   			ccnt = ccnt + 1
+		   			decky.logger.info(f"route to {r} deleted")
+			except subprocess.CalledProcessError as e:
+				decky.logger.info(e.stderr.decode())
+				pass
+		try:
+			bworld = subprocess.run(shlex.split("podman stop xivomega"),check=True,capture_output=True)
+			if bworld.returncode == 0:
+				ccnt = ccnt + 1
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+		try:
+			bworld = subprocess.run(shlex.split("podman rm xivomega"),check=True,capture_output=True)
+			if bworld.returncode == 0:
+				ccnt = ccnt + 1
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+		try:
+			flame = subprocess.run(shlex.split("podman network rm xivlanc"),check=True,capture_output=True)
+			if flame.returncode == 0:
+				ccnt = ccnt + 1
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+		try:
+			lanhdie = subprocess.run(shlex.split("ip link set xivlanh down"),check=True,capture_output=True)
+			if lanhdie.returncode == 0:
+				ccnt = ccnt + 1
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+		try:
+			lanhrm = subprocess.run(shlex.split("ip link del xivlanh"),check=True,capture_output=True)
+			if lanhrm.returncode == 0:
+				ccnt = ccnt +1
+		except subprocess.CalledProcessError as e:
+			pass
+		if (ccnt > 0):
+			decky.logger.info("Dangling elements from previous play session detected. CleanUp Protocol Activated and Completed")
+#testMethods - these will be static
+	def testStart(self):
+		try:
+			tworld = subprocess.run(shlex.split("podman start xivtest"),check=True,capture_output=True)
+			if tworld.returncode == 0:
+				decky.logger.info("podman started successfully")
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())			
+	def podmanInfo(self):
+		try:
+			pworld = subprocess.run(shlex.split("podman container ls -a"),check=True,capture_output=True)
+			nworld = subprocess.run(shlex.split("podman network ls"),check=True,capture_output=True)
+			if pworld.returncode == 0:
+				decky.logger.info(pworld.stdout.decode())
+			if nworld.returncode == 0:
+				decky.logger.info(nworld.stdout.decode())
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+
+	def testStop(self):
+		try:
+			tworld = subprocess.run(shlex.split("podman stop xivtest"),check=True,capture_output=True)
+			if tworld.returncode == 0:
+				decky.logger.info("podman stopped successfully")
+		except subprocess.CalledProcessError as e:
+			pass
+			decky.logger.info(e.stderr.decode())
+		# try:
+		# 	tworld = subprocess.run(shlex.split("podman rm xivomega"),check=True,capture_output=True)
+		# 	if tworld.returncode == 0:
+		# 		decky.logger.info("podman removed successfully")
+		# except subprocess.CalledProcessError as e:
+		# 	pass
+		# 	decky.logger.info(e.stderr.decode())
+
+class RootRequiredError(RuntimeError):
+	pass
+
+class InvalidIPException(Exception):
+	pass
+
+class ConnectionFailedError(Exception):
+	pass
+
+class NonExistentException(Exception):
+	pass
